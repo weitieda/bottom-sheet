6f0bc6b1ec3df871b03f9fa262e71d951683e698
Thomas Mellenthin <thomas.mellenthin@teufel.de>
2022-04-28 12:02:37 +0200

    Code review feedback, simplify condition

--- Sources/BottomSheet/BottomSheet.swift
+++ Sources/BottomSheet/BottomSheet.swift
@@ -91,9 +91,8 @@ public struct BottomSheet<Content: View>: View {
         .gesture(
             DragGesture()
                 .onChanged({ (value) in
-                    // ignore the swipe-up gesture which could collide with the iOS-global "go to homescreen"-gesture
-                    let isSwipeUp = value.startLocation.y > value.location.y
-                    if isPresented == false, isSwipeUp { return }
+                    // ignore gestures while the sheet is hidden. Otherwise it could collide with the iOS-global "go to homescreen"-gesture
+                    guard isPresented else { return }
 
                     let offsetY = value.translation.height
                     self.draggedOffset = offsetY
